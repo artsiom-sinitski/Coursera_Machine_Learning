6c297b2cedf618ebb51c661c2d56f4e8cfb38229
Artsiom Sinitski <artsiom.vs@gmail.com>
2019-04-16 17:25:07 +0300

    Coursera "Machine Learning" HW week 3

---
+++ machine-learning-ex3/ex3/predict.m
@@ -0,0 +1,27 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+
+gz2 = sigmoid(X * Theta1');
+gz2 = [ones(m, 1) gz2];
+
+hx = sigmoid(gz2 * Theta2');
+
+[maxnum, p] = max(hx, [], 2);
+
+end
